412a4dce9feb14da8a29e4c862ff6532399642b0
usama-ayub <usamaayub2012@gmail.com>
2017-05-23 06:32:16 -0700

    post schema update

--- dist/app/model/post.js
+++ dist/app/model/post.js
@@ -7,7 +7,9 @@ var PostSchema = new Schema({
     description: String,
     imgName: String,
     category: String,
+    feature: Boolean,
     createBy: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    price: Number,
     isLike: Boolean,
     createTime: {
         type: Date,
